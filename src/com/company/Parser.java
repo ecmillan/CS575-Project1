bbc88e5888af2fea99fc0ddd42f3ca2b41df7159
MEGATRON\Chris <ecmillan@crimson.ua.edu>
2016-10-23 01:34:03 -0500

    updated reference names

--- src/com/company/Parser.java
+++ src/com/company/Parser.java
@@ -8,8 +8,7 @@ import java.util.ArrayList;
 import java.util.Stack;
 
 
-import lexer.Token;
-import lexer.Lexer.Token.TokenType;
+
 
 /**
  * @author Ahmed
@@ -21,9 +20,9 @@ public class Parser {
 
     private Stack<String> NameStack;
 
-    private int indents; // maybe add indents too?
 
-    public void parse(List<Token> tokens) throws ParserException {
+
+    public void parse(List<Token> tokens) throws Exception {
         this.tokens = new ArrayList<Token>(tokens);
         this.NameStack = new Stack<String>();
         // List<Token> clone = new ArrayList<Token>(this.tokens);
@@ -33,40 +32,40 @@ public class Parser {
         document();
 
         if (!check(Token.TokenType.EPSILON)) {
-            throw new ParserException("Unexpected %s symbol found", lookahead);
+            throw new Exception("Unexpected " + lookahead + " symbol found");
         } else {
             System.out.println("Document parsed successfully!");
         }
     }
 
-    private void document() throws ParserException {
+    private void document() throws Exception {
         // document ::= element EOF
         System.out.println("document --> element EOF");
         element();
         match(Token.TokenType.EOF);
     }
 
-    private void element() throws ParserException {
+    private void element() throws Exception {
         // element ::= < elementPrefix
         // indent();
         System.out.println("element --> < elementPrefix");
-        match(Token.TokenType.OPEN);
+        match(Token.TokenType.OPEN_TAG);
         elementPrefix();
     }
 
-    private void elementPrefix() throws ParserException {
+    private void elementPrefix() throws Exception {
         // elementPrefix ::= NAME attribute elementSuffix
         System.out.println("elementPrefix --> NAME attribute elementSuffix");
         Token current = this.tokens.get(0);
-        //System.out.println("current elementPrefix  - " + current.getLexeme());
-        this.NameStack.push(current.getLexeme());
-        System.out.println("Opening tag - " + current.getLexeme());
+        //System.out.println("current elementPrefix  - " + current.value);
+        this.NameStack.push(current.value);
+        System.out.println("Opening tag - " + current.value);
         match(Token.TokenType.NAME);
         attribute();
         elementSuffix();
     }
 
-    private void attribute() throws ParserException {
+    private void attribute() throws Exception {
         // attribute ::= NAME = STRING attribute
         if (check(Token.TokenType.NAME)) {
             System.out.println("attribute --> NAME = STRING attribute");
@@ -80,30 +79,30 @@ public class Parser {
         }
     }
 
-    private void elementSuffix() throws ParserException {
+    private void elementSuffix() throws Exception {
         // elementSuffix ::= > elementOrData endTag
-        if (check(Token.TokenType.CLOSE)) {
+        if (check(Token.TokenType.CLOSE_TAG)) {
             System.out.println("elementSuffix --> > elementOrData endTag");
-            match(Token.TokenType.CLOSE);
+            match(Token.TokenType.CLOSE_TAG);
             elementOrData();
             endTag();
-        } else if (check(Token.TokenType.SLGT)) {
+        } else if (check(Token.TokenType.CLOSE_EMPTY_TAG)) {
             // elementSuffix ::= />
             System.out.println("elementSuffix --> />");
             // Since this is a Closing empty tag, auto pop the stack
             this.NameStack.pop();
             System.out.println("Closed empty tag");
-            match(Token.TokenType.SLGT);
+            match(Token.TokenType.CLOSE_EMPTY_TAG);
         } else {
-            throw new ParserException("Snytax error: An unexpected symbol has been encountered!");
+            throw new Exception("Snytax error: An unexpected symbol has been encountered!");
         }
     }
 
-    private void elementOrData() throws ParserException {
+    private void elementOrData() throws Exception {
         // elementOrData ::= < elementPrefix elementOrData
-        if (check(Token.TokenType.OPEN)) {
+        if (check(Token.TokenType.OPEN_TAG)) {
             System.out.println("elementOrData --> < elementPrefix elementOrData");
-            match(Token.TokenType.OPEN);
+            match(Token.TokenType.OPEN_TAG);
             elementPrefix();
             elementOrData();
         } else if (check(Token.TokenType.DATA)) {
@@ -117,25 +116,25 @@ public class Parser {
         }
     }
 
-    private void endTag() throws ParserException {
+    private void endTag() throws Exception {
         System.out.println("endTag --> </ NAME >");
         // 3 tokens here, so we need the middle one for name
         Token current = this.tokens.get(1);
-        //System.out.println("current end = |" + current.getLexeme() + "|");
+        //System.out.println("current end = |" + current.value + "|");
         // try to pop the string and see if they match
         String openName = this.NameStack.pop();
 
-        if (!openName.equals(current.getLexeme())){
-            System.out.println("Error - Invalid closing tag name. Expected " + openName + " but found " + current.getLexeme());
+        if (!openName.equals(current.value)){
+            System.out.println("Error - Invalid closing tag name. Expected " + openName + " but found " + current.value);
             // todo - throw error up to end program execution
             // since it is still broken and doesn't close program, just keep going for now
         }
         else {
             System.out.println("success closing " + openName);
         }
-        match(Token.TokenType.LTSL);
+        match(Token.TokenType.OPEN_CLOSING_TAG);
         match(Token.TokenType.NAME);
-        match(Token.TokenType.CLOSE);
+        match(Token.TokenType.CLOSE_TAG);
     }
 
 	/* From this point onward, we only have helper functions. */
@@ -144,31 +143,27 @@ public class Parser {
         this.tokens.remove(0);
 
         if (this.tokens.isEmpty()) {
-            lookahead = new TToken(Token.TokenType.EPSILON, "");
+            lookahead = new Token(Token.TokenType.EPSILON, "");
         } else {
             lookahead = this.tokens.get(0);
         }
     }
 
     private boolean check(Token.TokenType type) {
-        return lookahead.getType() == type;
+        return lookahead.type == type;
     }
 
-    private void match(Token.TokenType type) throws ParserException {
+    private void match(Token.TokenType type) throws Exception {
         matchNoAdvance(type);
 
         nextToken(); // advance to the next token
     }
 
-    private void matchNoAdvance(Token.TokenType type) throws ParserException {
+    private void matchNoAdvance(Token.TokenType type) throws Exception {
         if (!check(type)) {
-            throw new ParserException("Snytax error: An unexpected symbol has been encountered!");
+            throw new Exception("Snytax error: An unexpected symbol has been encountered!");
         }
     }
 
-    private void indent() {
-        for (int i = 0; i < indents; i++) {
-            System.out.print("\t");
-        }
-    }
+
 }
